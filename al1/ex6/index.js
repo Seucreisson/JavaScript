e1fed8ed58dafdba44945ae857bf7eb2c40d8bfa
Perseu <perseutub1@gmail.com>
2025-04-18 00:24:49 -0300

    km

---
+++ al1/ex6/index.js
@@ -0,0 +1,10 @@
+const alunos = 88;
+const prof = 5;
+const total = alunos + prof;
+const totalEscolas = total * 5;
+
+console.log(`o total de alunos é de: ${alunos} `)
+console.log(`o total de professores são: ${prof} `)
+console.log(`o total de alunos e professores são: ${total} `)
+console.log(`existem 5 escolas com a mesma quantidades de professores e alunos `)
+console.log(`o total de todas as escolas são: ${totalEscolas} `)
